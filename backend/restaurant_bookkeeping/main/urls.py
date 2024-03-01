6462bdc4b6c9c06002632ad11953e6d33d692d31
Johnny Nguyen <cuongdn2001@gmail.com>
2024-03-01 23:29:33 +1100

    Update expense category views

--- backend/restaurant_bookkeeping/main/urls.py
+++ backend/restaurant_bookkeeping/main/urls.py
@@ -4,5 +4,8 @@ from main.views import expense_category_views, index
 
 urlpatterns = [
   path('', index.index),
-  path('expenses/categories/', expense_category_views.ExpenseCategoryAPIView.as_view())
+  path('expenses/categories/', expense_category_views.ExpenseCategoryAPIView.as_view()),
+  path('expenses/categories/<int:id>/', expense_category_views.ExpenseCategoryAPIView.as_view(), name='api_specific_expense_category'),
+  path('expenses/categories/<int:id>/delete/', expense_category_views.ExpenseCategoryAPIView.as_view()),
+
 ]
--- backend/restaurant_bookkeeping/main/views/expense_category_views.py
+++ backend/restaurant_bookkeeping/main/views/expense_category_views.py
@@ -3,7 +3,11 @@ from django.http import JsonResponse, HttpResponse
 from django.views import View
 from django.views.decorators.csrf import csrf_exempt
 from django.utils.decorators import method_decorator
-from ..models import ExpenseCategory
+
+from django.db import IntegrityError
+from django.core.exceptions import ValidationError
+
+from ..models import ExpenseCategory, MainCategory
 
 @method_decorator(csrf_exempt, name='dispatch')  # Disable CSRF for simplicity
 class ExpenseCategoryAPIView(View):
@@ -12,7 +16,7 @@ class ExpenseCategoryAPIView(View):
         return {
             'id': object.id,
             'name': object.name,
-            'djandescription': object.description,
+            'description': object.description,
             'main_category': object.main_category
         }
     
@@ -34,21 +38,81 @@ class ExpenseCategoryAPIView(View):
 
     def post(self, request, *args, **kwargs):
         try:
+            print(f'request body {request.body}')
+            print(f'Request path {request.path}')
+            if request.path.endswith('/delete/'):
+                print('delete route')
+                print('kwargs ', kwargs)
+                return self.handle_delete(request, *args, **kwargs)
+
+            if (request.body is None):
+                return JsonResponse(data={'error': 'Invalid Request'}, status=400)
             data = json.loads(request.body)
 
             if data.get('name') is None:
                 return JsonResponse(data={'error': 'Name is required'}, status=400)
+            
+            data_main_category = data.get('main_category').upper()
 
-            category = ExpenseCategory.objects.create(
-                name=data['name'],
-                description=data.get('description', ''),  # Use .get to avoid KeyError
-                main_category=data['main_category']
-            )
-            return JsonResponse({
-                'id': category.id,
-                'name': category.name,
-                'description': category.description,
-                'main_category': category.main_category
-            }, status=201)  # Return HTTP 201 Created
+            valid_expense_categories = dict(MainCategory.MAIN_CATEGORIES_CHOICES).keys()
+            if data_main_category not in valid_expense_categories:
+                return JsonResponse({'error': 'Invalid Expense Category. Options are (operating, inventory, labour, others)'})
+            
+            if 'id' not in data:
+              category = ExpenseCategory.objects.create(
+                  name=data['name'],
+                  description=data.get('description', ''),
+                  main_category=data_main_category
+              )
+              return JsonResponse({
+                  'id': category.id,
+                  'name': category.name,
+                  'description': category.description,
+                  'main_category': category.main_category
+              }, status=201)
+            
+            if 'id' in data and data['id']:
+              try:
+                  category = ExpenseCategory.objects.get(id=data['id'])
+              except ExpenseCategory.DoesNotExist:
+                  return JsonResponse({'error': 'ExpenseCategory not found.'}, status=404)
+              
+              category.name = data['name']
+              category.description = data.get('description', '')
+              category.main_category = data_main_category
+              category.save()
+
+              return JsonResponse({
+                  'id': category.id,
+                  'name': category.name,
+                  'description': category.description,
+                  'main_category': category.main_category
+              }, status=200)
+
+        except IntegrityError as e:
+            print('error integrity', e)
+            return JsonResponse({'error': 'Database Integrity Error'}, status=400)
+        except ValidationError as e:
+            error_message = ', '.join(e.messages)
+            return JsonResponse({'error': f'{error_message}'}, status=400)
         except (ValueError, KeyError) as e:
-            return JsonResponse(data={'error': e}, status=400)  # Bad Request
+            print('error creating', e)
+            return JsonResponse(data={'error': e}, status=400)
+      
+    def handle_delete(self, request, *args, **kwargs):
+      try:
+          category_id = kwargs.get('id')
+          if category_id is None:
+              return JsonResponse({'error': 'ID is required for deletion'}, status=400)
+
+          try:
+              category = ExpenseCategory.objects.get(id=category_id)
+              category.delete()
+              return JsonResponse({'message': 'ExpenseCategory deleted successfully'}, status=200)
+          except ExpenseCategory.DoesNotExist:
+              return JsonResponse({'error': 'ExpenseCategory not found.'}, status=404)
+
+      except (ValueError, KeyError) as e:
+          return JsonResponse({'error': str(e)}, status=400)
+        
+      

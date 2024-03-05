c469ee83cef8ef75074e03d04d46bb368a7ce609
Johnny Nguyen <cuongdn2001@gmail.com>
2024-03-05 11:52:56 +1100

    Update main urls

--- backend/restaurant_bookkeeping/main/urls.py
+++ backend/restaurant_bookkeeping/main/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from main.views import expense_category_views, outlet_views, index
+from main.views import expense_category_views, outlet_views, payment_account_views, payee_views, index
 
 urlpatterns = [
   path('', index.index),
@@ -10,5 +10,11 @@ urlpatterns = [
 
   path('outlets/', outlet_views.OutletView.as_view()),
   path('outlets/<int:id>/', outlet_views.OutletView.as_view()),
+
+  path('payments/', payment_account_views.PaymentAccountView.as_view()),
+  path('payments/<int:id>', payment_account_views.PaymentAccountView.as_view()),
+
+  path('payees/', payee_views.PayeeView.as_view()),
+  path('payees/<int:id>', payee_views.PayeeView.as_view())
   
 ]
